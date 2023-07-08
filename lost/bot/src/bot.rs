1761f8e02717243597fba34a5e340b127f645a26
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2023-07-09 07:11:26 +0800

    Clear old code

--- lost/bot/src/bot.rs
+++
@@ -1,9 +0,0 @@
-use priconne_core::RegexTagger;
-
-pub struct Bot<C: Clone + Send> {
-    pub client: C,
-    pub database: db::Db,
-    pub telegraph: telegraph_rs::Telegraph,
-    pub bot: teloxide::adaptors::AutoSend<teloxide::Bot>,
-    pub tagger: RegexTagger,
-}
